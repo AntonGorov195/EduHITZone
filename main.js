c84a90f78428ccb20736fe19f5873a3a1f88bc39
AntonGorov195 <91376550+AntonGorov195@users.noreply.github.com>
2024-04-24 14:06:22 +0300

    moved files around, added the content page.

--- main.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-	const mainElem = document.getElementById("page-content");
-	mainElem.classList.add("start-anim");
-});
---
+++ public/contentPage/content.js
@@ -0,0 +1,8 @@
+// const isGuest = new URL(document.URL).searchParams.has("user", "guest");
+// 
+// const userNameBox = document.getElementById("user-name-box");
+// if (isGuest) {
+// 	userNameBox.innerText = "שלום אורח!";
+// } else {
+//     console.error("Getting user's name is not implemented.");
+// }
