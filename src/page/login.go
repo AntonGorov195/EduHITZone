fb8354efc145cb3659af9338cdc9951af70b1f86
AntonGorov195 <91376550+AntonGorov195@users.noreply.github.com>
2024-05-07 20:43:18 +0300

    Tidied up the login page.

--- src/page/login.go
+++ src/page/login.go
@@ -6,37 +6,47 @@ import (
 	"net/http"
 )
 
-type Data struct {
-	Name string
-}
-
 func AddHandles() {
+	// The initial data for the login page.
+	type LoginPagInitData struct {
+		ErrId string
+	}
+	// The ID of the element that will contain the message send by the form.
+	const ErrElemId = "error-msg"
 	http.HandleFunc("/login/login.html", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Hello, world!")
 		tmpl, _ := template.ParseFiles("public/login/login.html")
-		tmpl.Execute(w, 1)
+
+		tmpl.Execute(w, LoginPagInitData{ErrElemId})
 	})
 
-	http.HandleFunc("/tmp/1", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/api/v1/login/form", func(w http.ResponseWriter, r *http.Request) {
+		// Contains the data needed for the error message element.
+		type ErrorMessageData struct {
+			Name string
+		}
+
 		r.ParseForm()
+		const success = "<span id=\"" + ErrElemId + "\" style=\"color:green;\">Hello {{ .Name }}! Later this will look into the database and check password</span>"
+		const failed = "<span id=\"" + ErrElemId + "\" style=\"color:red;\">Try again, {{ .Name }}! Your name is not cool</span>"
 
 		name := r.Form.Get("name")
 		if name == "Ofek" || name == "Anton" {
-			tmpl, err := template.New("tmp").Parse("Hello {{.Name}}! Later this will look into the database and check password")
+			tmpl, err := template.New("cool_name").Parse(success)
 			if err != nil {
 				panic(err)
 			}
-			err = tmpl.Execute(w, Data{Name: r.Form.Get("name")})
+			err = tmpl.Execute(w, ErrorMessageData{Name: name})
 			if err != nil {
 				panic(err)
 			}
 			return
 		}
-		tmpl, err := template.New("tmp").Parse("Try again, {{.Name}}! Your name is not cool :(")
+		tmpl, err := template.New("invalid_name").Parse(failed)
 		if err != nil {
 			panic(err)
 		}
-		err = tmpl.Execute(w, Data{Name: r.Form.Get("name")})
+		err = tmpl.Execute(w, ErrorMessageData{Name: name})
 		if err != nil {
 			panic(err)
 		}
