45d8af3219d3c9431f94f1924bcca291e59c940f
AntonGorov195 <91376550+AntonGorov195@users.noreply.github.com>
2024-05-28 16:23:52 +0300

    Reverted captcha for now. Started working on error handling.

--- src/page/content.go
+++ src/page/content.go
@@ -1,25 +1,12 @@
 package spa
 
 import (
-	"bytes"
 	"database/sql"
-	"fmt"
-	"html/template"
 	"net/http"
 )
 
 func addContentHandle(db *sql.DB) {
 	http.HandleFunc("/content", func(w http.ResponseWriter, r *http.Request) {
-		var buf bytes.Buffer
-		tmpl, err := template.ParseFiles("SPAPublic/static/views/content.html")
-		if err != nil {
-			fmt.Println("Failed to parse content.html template")
-			panic(err.Error())
-		}
-		if err := tmpl.Execute(&buf, nil); err != nil {
-			fmt.Println("Failed to execute content.html template")
-			panic(err.Error())
-		}
-		Render(w, r, buf)
+		drawView(w, r, "content")
 	})
 }
--- src/page/index.go
+++ src/page/index.go
@@ -8,11 +8,11 @@ import (
 func addIndexHandle(db *sql.DB) {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path != "/" {
-			DrawView(w, r, "error")
+			drawView(w, r, "error")
 			return
 		}
 
-		DrawView(w, r, "entry")
+		drawView(w, r, "entry")
 	})
 }
 
