96f62beb897212398b07aa73aace90b9de4113f4
AntonGorov195 <91376550+AntonGorov195@users.noreply.github.com>
2024-05-31 20:16:19 +0300

    Now you can add dynamic courses to the content page

--- src/page/content.go
+++ src/page/content.go
@@ -1,12 +1,27 @@
 package spa
 
 import (
+	"bytes"
 	"database/sql"
+	"html/template"
 	"net/http"
 )
 
 func addContentHandle(db *sql.DB) {
 	http.HandleFunc("/content", func(w http.ResponseWriter, r *http.Request) {
-		drawView(w, r, "content")
+		// hitdb.GetCourses(db)
+		courses_names := []string{"Let the", " hit the"}
+
+		var buf bytes.Buffer
+		tmpl, err := template.ParseFiles("SPAPublic/static/views/content.html")
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		if err := tmpl.ExecuteTemplate(&buf, "primary", courses_names); err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		sendViewBuf(w, r, buf)
 	})
 }
