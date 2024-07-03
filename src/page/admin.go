6e311931f4f518ed4488e557cace336e7f511ad6
AntonGorov195 <91376550+AntonGorov195@users.noreply.github.com>
2024-07-03 18:11:45 +0300

    Reworked most of the css and html on the way...

--- src/page/admin.go
+++ src/page/admin.go
@@ -2,6 +2,7 @@ package spa
 
 import (
 	hitdb "EduHITZone/src/MySQL"
+	"bytes"
 	"database/sql"
 	"html/template"
 	"net/http"
@@ -9,7 +10,17 @@ import (
 
 func addAdminHandle(db *sql.DB) {
 	http.HandleFunc("/admin", func(w http.ResponseWriter, r *http.Request) {
-		drawView(w, r, "admin")
+		courses := hitdb.GetCourses(db)
+
+		var buf bytes.Buffer
+		tmpl, err := template.ParseFiles("public/static/views/admin.html", "public/static/views/course-list.html")
+		if err != nil {
+			panic(err)
+		}
+		if err := tmpl.Execute(&buf, courses); err != nil {
+			panic(err)
+		}
+		sendViewBuf(w, r, buf)
 	})
 	http.HandleFunc("/api/v1/admin/form", func(w http.ResponseWriter, r *http.Request) {
 		r.ParseForm()
